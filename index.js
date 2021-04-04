de2204220653b0356270c9b3e3d6841f1b3ac45e
rubabanat <rubabanat1996@gmail.com>
2021-04-04 22:03:02 +0300

    edit

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ require('dotenv').config();
 
 mongoose
 .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => { server.start(3000)
+  .then(() => { server.start(process.env.PORT)
   })
   .catch(e => console.error('Could not start server', e.message));
